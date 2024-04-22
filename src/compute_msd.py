ba444844da500670b8a15f64d9676ccb59ecbf19
Yuan Tang <yuantang@g.harvard.edu>
2024-04-22 18:44:18 -0400

    Update compute_msd.py

--- src/compute_msd.py
+++ src/compute_msd.py
@@ -1,23 +1,25 @@
 import argparse
 import torch
 from torchvision import transforms
+from tqdm import tqdm
 from utils import create_dataloader_from_preprocessed
 
 def main():
     parser = argparse.ArgumentParser(description='Process dataset arguments')
-    parser.add_argument('--data_dir', type=str, default='E:\synthetic_kids', help='Path to the dataset directory')
+    parser.add_argument('--data_dir', type=str, default="E:/depth_None_True/testset.pkl", help='Path to the dataset directory')
     args = parser.parse_args()
 
     # Define a transform to convert images to tensors
     to_tensor = transforms.Compose([transforms.ToTensor()])
     
+    print('...loading full dataset')
     loader = create_dataloader_from_preprocessed(args.data_dir, 1, transform = to_tensor)
-    print('Data successfully loaded from the pickle file.')
+    print('...data successfully loaded from the pickle file.')
 
     mean = torch.zeros(3)
     std = torch.zeros(3)
 
-    for data, _ in loader:
+    for data, _ in tqdm(loader, desc='Progress'):
         mean += data.mean([0, 2, 3])
         std += data.std([0, 2, 3])
 
@@ -28,3 +30,5 @@ def main():
     print(f'Mean: {mean}')
     print(f'Std: {std}')
 
+if __name__=='__main__':
+    main()
