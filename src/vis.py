fb1795a43fad8d4b5958bb460bbd8dcf47ac69f9
Yuan Tang <yuantang@g.harvard.edu>
2024-04-22 21:24:15 -0400

    Create vis.py

---
+++ src/vis.py
@@ -0,0 +1,22 @@
+import os
+import argparse
+from utils import *
+import matplotlib.pyplot as plt
+import numpy as np
+from torchvision import transforms
+from scipy.ndimage import gaussian_filter1d
+from sklearn.metrics import r2_score
+
+def main():
+    parser = argparse.ArgumentParser(description='Result Visualization')
+    parser.add_argument('--session_path', type=str, required=False, help='Path for the session')
+    args = parser.parse_args()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
