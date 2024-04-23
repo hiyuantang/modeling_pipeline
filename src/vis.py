e8fa57c4d9b17282ab87d456d09b9e22bf5d7d46
Yuan Tang <yuantang@g.harvard.edu>
2024-04-23 15:48:26 -0400

    Update vis.py

--- src/vis.py
+++ src/vis.py
@@ -4,20 +4,15 @@ import argparse
 from utils import *
 import matplotlib.pyplot as plt
 import numpy as np
-from torchvision import transforms
 from scipy.ndimage import gaussian_filter1d
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 
-def log_vis(*arg, save_dir, ylim=[0, 100], key='test', epochs=100, gaussian_smooth=True, sigma=4):
+def log_vis(session_path_list, save_dir, ylim=[0, 100], key='val', epochs=10, gaussian_smooth=True, sigma=3):
     plt.figure(figsize=(5,5))
 
-    for path_ in arg:
+    for path_ in session_path_list:
         cur_info_path = os.path.join(path_, 'info.json')
-
-        if key == 'test':
-            cur_log_path = os.path.join(path_, 'test_log.json')
-        else: 
-            cur_log_path = os.path.join(path_, 'train_log.json')
+        cur_log_path = os.path.join(path_, 'train_log.json')
 
         log_curve = json2dict(cur_log_path)[f'{key}_loss'][0:epochs]
         log_label = json2dict(cur_info_path)['model_name']
@@ -40,45 +35,37 @@ def log_vis(*arg, save_dir, ylim=[0, 100], key='test', epochs=100, gaussian_smoo
 def pred_vis(session_path, save_dir):
     pred_info_path = os.path.join(session_path, 'pred.json')
     model_info_path = os.path.join(session_path, 'info.json')
-    test_log_path = os.path.join(session_path, 'test_log.json')
 
     pred_info = json2dict(pred_info_path)
     model_info = json2dict(model_info_path)
-    test_log =  json2dict(test_log_path)
     model_name = model_info['model_name']
-    test_loss = test_log['test_loss'][0] 
 
     gt = np.array(pred_info['true_labels']).reshape(1, -1)
     pred = np.array(pred_info['predictions']).reshape(1, -1)
 
-    r_squared = r2_score(gt.reshape(-1, 1), pred.reshape(-1, 1))
-    accuracy = np.mean(np.abs(gt - pred) <= 1)
-    print(f'R^2 Score: {r_squared:.3f}')
-    print(f'Accuracy within ±1 unit: {accuracy:.3f}')
-    print(f'Loss: {test_loss:.3f}')
-
     # Plot
     plt.figure(figsize=(5,5))
     plt.scatter(gt, pred, c='pink', alpha=0.3)
-    plt.title(f'{model_name}')
+    plt.title(f'{model_name} Testset Ground Truth vs Prediction')
 
     # Create a line space for the ideal line where ground truth equals predictions
-    line_space = np.linspace(min(np.minimum(gt, pred)), max(np.maximum(gt, pred)), 30)
-    plt.plot(line_space, line_space, 'r', alpha=0.3, label='Ideal: y = x')  # Red dashed line
-    plt.plot(line_space, line_space + 1, 'b', alpha=0.3, label='Error Margin +1')  # Blue dashed line for +1 error margin
-    plt.plot(line_space, line_space - 1, 'b', alpha=0.3, label='Error Margin -1')  # Blue dashed line for -1 error margin
+    line_space = np.linspace(58, 88, 30)
+    plt.plot(line_space, line_space, 'r', alpha=1, label='Ideal: pred = truth')  # Red dashed line
+    plt.plot(line_space, line_space + 1, 'b', alpha=1, label='Error Margin +-1')  # Blue dashed line for +1 error margin
+    plt.plot(line_space, line_space - 1, 'b', alpha=1)  # Blue dashed line for -1 error margin
 
     plt.xlabel('Ground Truth')
     plt.ylabel('Prediction')
+    plt.legend()
     plt.tight_layout()
 
     # Save the plot
     plt.savefig(os.path.join(save_dir, f'{model_name}_gt_pred_plot.png'))
 
-def metric_vis(result_path_list, metric):
+def metric_vis(session_path_list, metric, save_dir, ylim=None):
     pred_info_path_list = []
     model_info_path_list = []
-    for session_path in result_path_list:
+    for session_path in session_path_list:
         pred_info_path = os.path.join(session_path, 'pred.json')
         model_info_path = os.path.join(session_path, 'info.json')
         pred_info_path_list.append(pred_info_path)
@@ -98,6 +85,12 @@ def metric_vis(result_path_list, metric):
         elif metric == 'acc':
             accuracy = np.mean(np.abs(gt - pred) <= 1)
             metric_list.append(accuracy)
+        elif metric == 'mse':
+            mse = mean_squared_error(gt, pred)
+            metric_list.append(mse)
+        elif metric == 'mae':
+            mae = mean_absolute_error(gt, pred)
+            metric_list.append(mae)
         else:
             print('Metric not yet supported.')
             return
@@ -108,44 +101,60 @@ def metric_vis(result_path_list, metric):
     sorted_metrics = np.array(metric_list)[sorted_indices]
     
     # Color settings
-    colors = ['crimson' if i == 0 else 'skyblue' for i in range(len(sorted_model_names))]
+    if metric == 'r2' or metric == 'acc':
+        colors = ['crimson' if i == 0 else 'skyblue' for i in range(len(sorted_model_names))]
+    elif metric == 'mse' or metric == 'mae':
+        colors = ['crimson' if i == len(session_path_list)-1 else 'skyblue' for i in range(len(sorted_model_names))]
     
     # Create the bar plot
     plt.figure(figsize=(5, 5))
     bars = plt.bar(sorted_model_names, sorted_metrics, color=colors)
     
     # Highlight the best model
-    bars[0].set_label('Best Model')
+    if metric == 'r2' or metric == 'acc':
+        bars[0].set_label('Best Model')
+    elif metric == 'mse' or metric == 'mae':
+        bars[len(session_path_list)-1].set_label('Best Model')
     plt.legend()
     
     plt.xlabel('Model Names')
-    plt.ylabel(metric.capitalize())
+    plt.ylabel(metric.upper())
+    if ylim != None:
+        plt.ylim(ylim)
     plt.title(f'Comparison of Models Based on {metric.upper()}')
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
 
-metric_vis(result_path_list, 'acc')
-metric_vis(result_path_list, 'r2')
+    # Save the plot
+    plt.savefig(os.path.join(save_dir, f'{metric}_plot.png'))
 
 def main():
     parser = argparse.ArgumentParser(description='Result Visualization')
-    parser.add_argument('--session_path', type=str, required=False, help='Path for a single session')
     parser.add_argument('--all', type=bool, default=True, help='All sessions in the results directory')
+    parser.add_argument('--session_path', type=str, required=False, help='Path for a single session')
     args = parser.parse_args()
 
+    save_dir = './vis'
+
     if args.all == True:
-        result_path_list = get_subdirpath_list()
-        log_vis(*result_path_list, key='train')
-        log_vis(*result_path_list, key='val')
-        log_vis(*result_path_list, key='test')
+        session_path_list = get_subdirpath_list()
 
+        # Plot for log info
+        log_vis(session_path_list, save_dir, key='train', ylim=[0,100])
+        log_vis(session_path_list, save_dir, key='val', ylim=[0,5])
 
+        # Plot for metrics
+        metric_vis(session_path_list, 'acc', save_dir)
+        metric_vis(session_path_list, 'r2', save_dir)
+        metric_vis(session_path_list, 'mse', save_dir)
+        metric_vis(session_path_list, 'mae', save_dir)
 
-
-
-
-
+        # Plot for ground truth vs prediction
+        for session_path in session_path_list: 
+            pred_vis(session_path, save_dir)
+    
+    if args.all == False:
+        pred_vis(args.session_path, save_dir)
 
 if __name__ == "__main__":
     main()
