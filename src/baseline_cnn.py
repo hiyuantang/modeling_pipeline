71974ce6b5f16e525ebf2921ed4d6add7a1f3d5b
Yuan Tang <yuantang@g.harvard.edu>
2024-04-24 03:57:14 -0400

    Create baseline_cnn.py

---
+++ src/baseline_cnn.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+
+class BaselineCNN(nn.Module):
+    def __init__(self):
+        super(BaselineCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.relu3 = nn.ReLU()
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.fc = nn.Linear(64 * 28 * 28, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+        
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+        
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+        
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
