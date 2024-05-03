41fe694a240f790db88259fed06e3c008ca59838
Yuan Tang <yuantang@g.harvard.edu>
2024-05-03 05:46:28 -0400

    Update test.py

--- src/test.py
+++ src/test.py
@@ -7,66 +7,82 @@ import json
 from utils import *
 
 def test(session_dir, test_data_dir, batch_size, drop_rate, device):
+    """
+    Perform inference on a test dataset using a pre-trained model.
 
+    This function loads a trained model and performs inference on a provided
+    test dataset. It computes the loss using Mean Squared Error (MSE) and
+    saves the inference results and loss values in JSON files.
+
+    Parameters:
+        session_dir (str): Directory where the session's files are stored.
+        test_data_dir (str): Directory containing the preprocessed test data.
+        batch_size (int): The size of the batch for the DataLoader.
+        drop_rate (float): The dropout rate used in the model.
+        device (str): The device to run the inference on ('cpu' or 'cuda').
+    """
+    # Paths to the model and info files
     model_path = os.path.join(session_dir, 'best_model.pth')
     info_path = os.path.join(session_dir, 'info.json')
+    # Load model information from the JSON file
     model_info = json2dict(info_path)
     
+    # Define the transformations for the test dataset
     test_transform = transforms.Compose([
-    transforms.ToTensor(), 
-    transforms.Resize((224, 224)),  
-    transforms.Normalize(mean=[0.3568, 0.3568, 0.3568], std=[0.3512, 0.3512, 0.3512]) 
+        transforms.ToTensor(), 
+        transforms.Resize((224, 224)),  
+        transforms.Normalize(mean=[0.3568, 0.3568, 0.3568], std=[0.3512, 0.3512, 0.3512]) 
     ])
 
+    # Load the test dataset
     print('...loading testing dataset')
     test_dataset = create_dataset_from_preprocessed(test_data_dir, test_transform)
 
-    # Create DataLoaders for each subset
+    # Create a DataLoader for the test dataset
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, pin_memory=True)
     print('...testing dataset loading completed')
 
-    # Load the model
+    # Load the model for inference
     model, _ = get_pretrained_model(model_info['model_name'], num_classes=1, drop_rate=drop_rate, batch_size=batch_size, pretrained=False)
     device = torch.device(device)
     model = model.to(device)
+    # Load the model weights
     model.load_state_dict(torch.load(model_path, map_location=device))
 
-    # Initialize a dictionary to store the testing loss value
+    # Initialize logs for storing loss and predictions
     loss_log = {'test_loss': []}
-
-    # Initialize a dictionary to store the predictions and true labels
     predictions_log = {'true_labels': [], 'predictions': []}
 
-    # Define the loss function and optimizer
+    # Define the loss function
     criterion = torch.nn.MSELoss()
 
-    # Inference
+    # Perform inference on the test dataset
     test_loss = 0.0
     model.eval()
     with torch.no_grad():
         for inputs, labels in tqdm(test_loader, desc='Inference Progress:'):
+            # Move inputs and labels to the specified device
             inputs, labels = inputs.to(device), labels.to(device).view(-1, 1)
+            # Get model predictions
             outputs = model(inputs)
+            # Compute the loss
             loss = criterion(outputs, labels)
             test_loss += loss.item()
 
-            # Store true labels and predictions
+            # Store true labels and model predictions
             predictions_log['true_labels'].extend(labels.tolist())
             predictions_log['predictions'].extend(outputs.tolist())
 
-    # Calculate average validation loss
+    # Calculate the average loss over all batches
     test_loss /= len(test_loader)
     print(f'Inference Loss: {test_loss:.4f}')
 
-    # Append the loss values for the current epoch to the loss_log dictionary
+    # Save the loss and predictions to JSON files
     loss_log['test_loss'].append(test_loss)
-
-    # Save the loss_log dictionary as a JSON file
     log_file_path = os.path.join(session_dir, 'test_log.json')
     with open(log_file_path, 'w') as log_file:
         json.dump(loss_log, log_file)
     
-    # Save the predictions_log dictionary as a JSON file
     pred_file_path = os.path.join(session_dir, 'pred.json')
     with open(pred_file_path, 'w') as pred_file:
         json.dump(predictions_log, pred_file)
