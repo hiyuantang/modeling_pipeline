76a4aab899ad87583a195af5fba63e0ecaa4b6c3
Yuan Tang <yuantang@g.harvard.edu>
2024-05-03 02:04:05 -0400

    Update baseline_cnn.py

--- src/baseline_cnn.py
+++ src/baseline_cnn.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 class BaselineCNN(nn.Module):
-    def __init__(self):
+    def __init__(self, drop_rate, num_classes):
         super(BaselineCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
@@ -15,7 +15,16 @@ class BaselineCNN(nn.Module):
         self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.fc = nn.Linear(64 * 28 * 28, 1)
+        num_features = 64 * 28 * 28
+        self.fc_sq = nn.Sequential(
+                    nn.Linear(num_features, 512),
+                    nn.ReLU(),
+                    nn.Dropout(drop_rate),
+                    nn.Linear(512, 256),
+                    nn.ReLU(),
+                    nn.Dropout(drop_rate),
+                    nn.Linear(256, num_classes)
+                    )
 
     def forward(self, x):
         x = self.conv1(x)
@@ -31,5 +40,5 @@ class BaselineCNN(nn.Module):
         x = self.pool3(x)
         
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.fc_sq(x)
         return x
