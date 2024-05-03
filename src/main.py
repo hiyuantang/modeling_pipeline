a001deb9ab9397ef5d26121b6bc37e9c14307259
Yuan Tang <yuantang@g.harvard.edu>
2024-05-03 05:55:35 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,12 +9,12 @@ from utils import *
 
 def main():
     parser = argparse.ArgumentParser(description='Modeling Pipeline.')
-    parser.add_argument('--model_name', type=str, default='vit_b_16', help='Name of the model')
+    parser.add_argument('--model_name', type=str, default='baseline_cnn', help='Name of the model')
     parser.add_argument('--train_data_dir', type=str, default='E:/depth_None_True/trainset.pkl', help='Directory of the training data')
     parser.add_argument('--test_data_dir', type=str, default='E:/depth_None_True/testset.pkl', help='Directory of the testing data')
-    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
+    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
-    parser.add_argument('--learning_rate', type=float, default=0.0001, help='Learning rate')
+    parser.add_argument('--learning_rate', type=float, default=0.00002, help='Learning rate')
     parser.add_argument('--drop_rate', type=float, default=0.1, help='Drop out rate')
     parser.add_argument('--pre_trained', type=bool, default=True, help='Pre-trained weights for the model')
     parser.add_argument('--device', type=str, default='auto', choices=['auto', 'cuda', 'mps', 'cpu'], help='Device to train and test on')
