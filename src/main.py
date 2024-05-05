8f9b6289f889f5d30635658f1bd480e73022faca
Yuan Tang <yuantang@g.harvard.edu>
2024-05-05 03:45:44 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def main():
     parser.add_argument('--model_name', type=str, default='baseline_cnn', help='Name of the model')
     parser.add_argument('--train_data_dir', type=str, default='E:/depth_None_True/trainset.pkl', help='Directory of the training data')
     parser.add_argument('--test_data_dir', type=str, default='E:/depth_None_True/testset.pkl', help='Directory of the testing data')
-    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs')
+    parser.add_argument('--epochs', type=int, default=5, help='Number of epochs')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
     parser.add_argument('--learning_rate', type=float, default=0.00002, help='Learning rate')
     parser.add_argument('--drop_rate', type=float, default=0.1, help='Drop out rate')
