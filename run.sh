2391d88fe2d58e0a4ea5d33d4c38509007409c4d
Yuan Tang <yuantang@g.harvard.edu>
2024-05-07 13:34:21 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 python src/main.py --model_name baseline_cnn
-python src/main.py --model_name resnet18
-python src/main.py --model_name resnet34
-python src/main.py --model_name resnet50
 python src/main.py --model_name mobilenet_v3_small
 python src/main.py --model_name mobilenet_v3_large
+python src/main.py --model_name resnet18
+python src/main.py --model_name resnet34
