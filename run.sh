a6e36ed5002cfdc04505dbefb9fad3262197f093
Yuan Tang <yuantang@g.harvard.edu>
2024-05-10 19:53:22 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 python src/main.py --model_name baseline_cnn
-python src/main.py --model_name mobilenet_v3_small
-python src/main.py --model_name mobilenet_v3_large
 python src/main.py --model_name resnet18
 python src/main.py --model_name resnet34
+python src/main.py --model_name resnet50
+python src/main.py --model_name resnet101
+python src/main.py --model_name resnet152
+python src/main.py --model_name vgg16
+python src/main.py --model_name vgg19
+python src/main.py --model_name vgg16_bn
+python src/main.py --model_name vgg19_bn
+python src/main.py --model_name mobilenet_v3_small
+python src/main.py --model_name mobilenet_v3_large
+python src/main.py --model_name vit_b_16
+python src/main.py --model_name vit_b_32
+python src/main.py --model_name vit_l_16
+python src/main.py --model_name vit_l_32
+python src/main.py --model_name vit_h_14
