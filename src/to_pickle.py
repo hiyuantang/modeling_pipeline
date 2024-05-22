faaedc8c9fcce84a25e974cfb0fd3224e45349d1
Yuan Tang <yuantang@g.harvard.edu>
2024-05-22 14:01:55 -0400

    Update to_pickle.py

--- src/to_pickle.py
+++ src/to_pickle.py
@@ -23,7 +23,7 @@ def main():
     parser.add_argument('--data_dir', type=str, default='E:/kaggle_height_weight', help='Path to the dataset directory')
     parser.add_argument('--data_name', type=str, choices=['synth', 'kagglehw'], default='kagglehw', help='Name of the dataset')
     parser.add_argument('--image_type', type=str, default='rgb', help='Type of image (e.g., depth, rgb, or segmentation)')
-    parser.add_argument('--gray_scale', type=bool, default=False, help='Convert the image to grayscale by averaging over RGB channels')
+    parser.add_argument('--gray_scale', type=str2bool, default=False, help='Convert the image to grayscale by averaging over RGB channels')
     parser.add_argument('--H_or_W', type=str, default='H', choices=['H', 'W'], help='Create dataset label as height or weight')
     parser.add_argument('--train_size', type=float, default=0.9, help='Proportion of dataset to use for training')
     args = parser.parse_args()
