1725dc21163282bf293662a96a72eff33d40a4fd
Yuan Tang <yuantang@g.harvard.edu>
2024-05-30 22:22:27 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-python src/main.py --model_name baseline_cnn
-python src/main.py --model_name resnet18
-python src/main.py --model_name resnet34
-python src/main.py --model_name resnet50
-python src/main.py --model_name resnet101
-python src/main.py --model_name resnet152
-python src/main.py --model_name vgg16
-python src/main.py --model_name vgg19
-python src/main.py --model_name vgg16_bn
-python src/main.py --model_name vgg19_bn
-python src/main.py --model_name mobilenet_v3_small
-python src/main.py --model_name mobilenet_v3_large
-python src/main.py --model_name vit_b_16
-python src/main.py --model_name vit_b_32
-python src/main.py --model_name vit_l_16
-python src/main.py --model_name vit_l_32
-python src/main.py --model_name vit_h_14
+python src/main.py --model_name baseline_cnn --epochs 100
+python src/main.py --model_name resnet18 --epochs 100
+python src/main.py --model_name resnet34 --epochs 100
+python src/main.py --model_name resnet50 --epochs 100
+python src/main.py --model_name resnet101 --epochs 100
+python src/main.py --model_name resnet152 --epochs 100
+python src/main.py --model_name vgg16 --epochs 100
+python src/main.py --model_name vgg19 --epochs 100
+python src/main.py --model_name vgg16_bn --epochs 100
+python src/main.py --model_name vgg19_bn --epochs 100
+python src/main.py --model_name mobilenet_v3_small --epochs 100
+python src/main.py --model_name mobilenet_v3_large --epochs 100
+python src/main.py --model_name vit_b_16 --epochs 100
+python src/main.py --model_name vit_b_32 --epochs 100
+python src/main.py --model_name vit_l_16 --epochs 100
+python src/main.py --model_name vit_l_32 --epochs 100
+python src/main.py --model_name vit_h_14 --epochs 100
